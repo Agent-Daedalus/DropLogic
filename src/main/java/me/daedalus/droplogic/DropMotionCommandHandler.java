86fc77c7684861d01977793b3ea6a5dd61d4b2b2
Daedalus <agentdumbledore@gmail.com>
2024-11-02 16:07:32 -0500

    added reroll command

--- src/main/java/me/daedalus/droplogic/DropMotionCommandHandler.java
+++ src/main/java/me/daedalus/droplogic/DropMotionCommandHandler.java
@@ -17,6 +17,8 @@ import net.minecraft.command.CommandRegistryAccess;
 
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.item.Item;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.command.CommandSource;
@@ -30,17 +32,44 @@ public class DropMotionCommandHandler {
         argument("location", BlockPosArgumentType.blockPos()).then(
           argument("item", ItemStackArgumentType.itemStack(registryAccess)).then(
             argument("offset", Vec3ArgumentType.vec3(false)).then(
-              argument("velocity", Vec3ArgumentType.vec3(false)).
-                executes(ctx -> 
-                  setDropMotion(ctx.getSource(), 
-                    BlockPosArgumentType.getBlockPos(ctx, "location"), 
-                    ItemStackArgumentType.getItemStackArgument(ctx, "item").getItem(), 
-                    Vec3ArgumentType.getVec3(ctx, "offset"),
-                    Vec3ArgumentType.getVec3(ctx, "velocity")
-                )
-              )
+              argument("velocity", Vec3ArgumentType.vec3(false)).executes(ctx -> setDropMotion(ctx.getSource(), 
+                BlockPosArgumentType.getBlockPos(ctx, "location"), 
+                ItemStackArgumentType.getItemStackArgument(ctx, "item").getItem(), 
+                Vec3ArgumentType.getVec3(ctx, "offset"),
+                Vec3ArgumentType.getVec3(ctx, "velocity")
+              ))
             )
           )
+        ).then(
+          literal("all").then(
+            argument("offset", Vec3ArgumentType.vec3(false)).then(
+              argument("velocity", Vec3ArgumentType.vec3(false)).executes(ctx -> setDropMotion(
+                ctx.getSource(), 
+                BlockPosArgumentType.getBlockPos(ctx, "location"), 
+                Items.AIR, 
+                Vec3ArgumentType.getVec3(ctx, "offset"),
+                Vec3ArgumentType.getVec3(ctx, "velocity")
+              ))
+            )
+          )
+        )
+      )
+    );
+
+    dispatcher.register(
+      literal("rerollDropMotion").then(
+        argument("location", BlockPosArgumentType.blockPos()).then(
+          argument("item", ItemStackArgumentType.itemStack(registryAccess)).executes(ctx -> rerollDropMotion(
+            ctx.getSource(),
+            BlockPosArgumentType.getBlockPos(ctx, "location"), 
+            ItemStackArgumentType.getItemStackArgument(ctx, "item").getItem()
+          ))
+        ).then(
+          literal("all").executes(ctx -> rerollDropMotion(
+            ctx.getSource(),
+            BlockPosArgumentType.getBlockPos(ctx, "location"), 
+            Items.AIR
+          ))
         )
       )
     );
@@ -48,7 +77,14 @@ public class DropMotionCommandHandler {
 
   public static int setDropMotion(ServerCommandSource source, BlockPos blockPos, Item item, Vec3d offset, Vec3d velocity) {
     DropVelocityMap.addDropMotion(blockPos, item, new DropMotion(offset.x, offset.y, offset.z, velocity.x, velocity.y, velocity.z));
-    source.sendFeedback(() -> Text.literal("BlockPos: " + blockPos.toString()), true);
+    source.sendFeedback(() -> Text.literal("Set " + blockPos.toShortString() + " for " + (item == Items.AIR ? "all" : item.getName()) 
+                + " to (offset=" + offset.toString() + ", velocity=" + velocity.toString() + ")"), true);
+    return Command.SINGLE_SUCCESS; // Return success
+  }
+
+  public static int rerollDropMotion(ServerCommandSource source, BlockPos blockPos, Item item) {
+    DropVelocityMap.addDropMotion(blockPos, item, new DropMotion(source.getWorld().random.nextLong()));
+    source.sendFeedback(() -> Text.literal("Rerolled at BlockPos: " + blockPos.toShortString() + " for " + (item == Items.AIR ? "all" : item.getName())), true);
     return Command.SINGLE_SUCCESS; // Return success
   }
 }
